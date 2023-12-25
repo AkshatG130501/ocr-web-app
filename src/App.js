0391c880b15d4bba499132817ebb371984b5fe5e
Akshat Gupta <guptaakshat370@gmai.com>
2023-12-25 13:14:21 +0530

    Heading : Font change , Spacing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Image To Text</h1>
+    <div className="container" style={{height:"100vh"}}>
+      <div className="row h-100">
+        <div className="col-md-5 mx-auto d-flex flex-column align-items-center">
+          <h1 className="mt-5 mb-4">Image To Text</h1>
+        </div>
+      </div>
     </div>
   );
 }
