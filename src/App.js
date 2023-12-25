4f31d8d2454d4a27a0d0206f5c6f7779843099ef
Akshat Gupta <guptaakshat370@gmai.com>
2023-12-25 12:56:44 +0530

    Modified : App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
+import react from "react";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image To Text</h1>
     </div>
   );
 }
